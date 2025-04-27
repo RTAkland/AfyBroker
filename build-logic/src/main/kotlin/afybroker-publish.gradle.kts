c6dc4b67390145683b11ddf6de26115b5b8790fa
RTAkland <rtakland@outlook.com>
2025-04-27 21:40:21 +0800

    Update maven repository

--- build-logic/src/main/kotlin/afybroker-publish.gradle.kts
+++ build-logic/src/main/kotlin/afybroker-publish.gradle.kts
@@ -5,8 +5,15 @@ plugins {
 
 extensions.configure<PublishingExtension> {
     repositories {
-        maven("http://repo.afyer.net/repository/maven-releases/") {
-            credentials(PasswordCredentials::class.java)
+//        maven("http://repo.afyer.net/repository/maven-releases/") {
+//            credentials(PasswordCredentials::class.java)
+//            isAllowInsecureProtocol = true
+//        }
+        maven("http://192.168.10.222:9098/releases") {
+            credentials {
+                username = "RTAkland"
+                password = System.getenv("PUBLISH_TOKEN")
+            }
             isAllowInsecureProtocol = true
         }
     }
